6ae456413c50d63f5119172fca4ce317b7b78f5a
JPPaião <jp.paiao03@gmail.com>
2023-05-26 22:58:57 -0300

    refactor: Refatorado componente de login

--- src/routes/login/index.jsx
+++ src/routes/login/index.jsx
@@ -1,35 +1,37 @@
-import { Link, Form, useActionData, useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
+import { Form, Link, useActionData, useNavigate } from "react-router-dom"
 import { Button } from "../../components/button"
+import { EmailIcon, EyeIcon, LockIcon } from "../../components/icons/icons"
 import { Inputs } from "../../components/inputs"
-import { connect } from "react-redux"
-import { EmailIcon, LockIcon, EyeIcon } from '../../components/icons/icons'
+import { store } from "../../store"
 
 async function actionLogin({ request }) {
+    const storeUser = store
     const formData = await request.formData()
     const updates = Object.fromEntries(formData)
-    const response = await fetch('http://127.0.0.1:5000/login', {
+    const response = await fetch("http://127.0.0.1:5000/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(updates)
+        body: JSON.stringify(updates),
     })
-    .then(r => r.json())
-    .catch(r => r.json())
+        .then((r) => r.json())
+        .catch((r) => r.json())
 
     if (response.Error) {
         return response
     }
 
+    storeUser.dispatch({ type: "user/login", payload: response.user })
     return response
 }
 
-function Login({ user, setUser }) {
-    const response = useActionData()
+function Login() {
+    const responseAction = useActionData()
     const navigate = useNavigate()
-    const [eye, setEye] = useState(false)
     const [error, setError] = useState({})
+    const [eye, setEye] = useState(false)
     const [inputs, setInputs] = useState({
         email: "",
         password: "",
@@ -37,38 +39,39 @@ function Login({ user, setUser }) {
 
     useEffect(() => {
         const handleLogin = () => {
-            if (response) {
-                if (response.Error) {
-                    setError(response)
+            if (responseAction) {
+                if (responseAction.Error) {
+                    setError(responseAction)
                 } else {
-                    setUser(response.user)
-                    navigate('/dashboard')
+                    navigate("/dashboard")
                 }
             }
-        }
+        };
         handleLogin()
-    }, [response])
+    }, [responseAction])
 
     return (
         <div className="flex gap-1 justify-between h-screen w-full text-black">
             <section className="p-6 w-full max-w-lg flex justify-center">
                 <div className="max-w-sm mx-auto w-full sm:m-auto">
-                    <h1 className="text-xl text-center font-bold text-black">Info Finance</h1>
+                    <h1 className="text-xl text-center font-bold text-black">
+                        Info Finance
+                    </h1>
                     <div className="mt-7">
-                        <h2 className="font-bold text-center text-4xl text-blue-900">Login</h2>
-                        {
-                            (error == {})
-                            ? (
-                                <>
-                                    {" "}
-                                </>
-                            ) : (
-                                <p className="pt-4 text-center text-xl text-red-700 font-bold">
-                                    {error.Error}
-                                </p>
-                            )
-                        }
-                        <Form method="post" className="flex flex-col gap-5 my-6">
+                        <h2 className="font-bold text-center text-4xl text-blue-900">
+                            Login
+                        </h2>
+                        {error == {} ? (
+                            <>{" "}</>
+                        ) : (
+                            <p className="pt-4 text-center text-xl text-red-700 font-bold">
+                                {error.Error}
+                            </p>
+                        )}
+                        <Form
+                            method="post"
+                            className="flex flex-col gap-5 my-6"
+                        >
                             <div className="relative">
                                 <EmailIcon className="absolute top-[38px] left-3 text-zinc-500" />
                                 <Inputs
@@ -77,7 +80,12 @@ function Login({ user, setUser }) {
                                     type={"email"}
                                     value={inputs.email}
                                     place={"Example@gmail.com"}
-                                    onChange={e => setInputs({...inputs, email: e.target.value})}
+                                    onChange={(e) =>
+                                        setInputs({
+                                            ...inputs,
+                                            email: e.target.value,
+                                        })
+                                    }
                                     className={"px-10 py-2"}
                                 />
                             </div>
@@ -89,20 +97,40 @@ function Login({ user, setUser }) {
                                     type={eye ? "text" : "password"}
                                     value={inputs.password}
                                     place={"*********"}
-                                    onChange={e => setInputs({...inputs, password: e.target.value})}
+                                    onChange={(e) =>
+                                        setInputs({
+                                            ...inputs,
+                                            password: e.target.value,
+                                        })
+                                    }
                                     className={"px-10 py-2"}
                                 />
                                 <span
                                     className="absolute top-[38px] right-3 cursor-pointer"
                                     onClick={() => setEye(!eye)}
                                 >
-                                    <EyeIcon eyes={eye} className={"text-zinc-500"} />
+                                    <EyeIcon
+                                        eyes={eye}
+                                        className={"text-zinc-500"}
+                                    />
                                 </span>
                             </div>
-                            <Button children={"Entre"} type={"submit"} className={"w-[90%] text-xl px-2 py-2"} />
+                            <Button
+                                children={"Entre"}
+                                type={"submit"}
+                                className={"w-[90%] text-xl px-2 py-2"}
+                            />
                         </Form>
                         <div className="text-center text-sm">
-                            <p className="text-zinc-800 font-semibold">Ainda não tem conta? <Link className="text-blue-900 hover:text-blue-600 font-bold" to="register">Cadastre-se</Link></p>
+                            <p className="text-zinc-800 font-semibold">
+                                Ainda não tem conta?{" "}
+                                <Link
+                                    className="text-blue-900 hover:text-blue-600 font-bold"
+                                    to="register"
+                                >
+                                    Cadastre-se
+                                </Link>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -118,17 +146,4 @@ function Login({ user, setUser }) {
     )
 }
 
-const mapStateToProps = state => {
-    return {
-        user: state.user
-    }
-}
-
-const mapDispatchToProps = dispatch => {
-    return {
-        setUser: (user) => dispatch({ type: "user/login", payload: user })
-    }
-}
-
-export { actionLogin }
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+export { Login, actionLogin }
